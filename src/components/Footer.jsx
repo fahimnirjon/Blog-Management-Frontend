d036bc5cd9a0290b3252c818b358a46b753fa29e
fahimnirjon <fahimnirjon21@gmail.com>
2024-10-28 01:35:19 +0600

    added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -117,7 +117,7 @@ const Footer = () => {
               </a>
             </li>
             <li>
-              <a href="/">
+              <a href="">
                 <FaFacebook className="w-6 h-auto" />
               </a>
             </li>
